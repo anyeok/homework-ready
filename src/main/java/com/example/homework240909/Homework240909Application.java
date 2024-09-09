a13fcbad548d6c1467a8a8d883389c4634000aa9
anyeok <https://github.com/anyeok>
2024-09-09 12:05:52 +0900

    start

---
+++ src/main/java/com/example/homework240909/Homework240909Application.java
@@ -0,0 +1,13 @@
+package com.example.homework240909;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework240909Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Homework240909Application.class, args);
+	}
+
+}
